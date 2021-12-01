1d18a2a42e5326b73b0e0bad777897452d6b66c8
Muhammad Afif Anwar Fauzi <anwarfauzi.maaf@gmail.com>
2021-12-01 16:25:10 +0800

    menambahkan list

--- index.php
+++ index.php
@@ -9,5 +9,9 @@
 <body>
     <h1>Hello World!</h1>
     <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Inventore ut facilis, repudiandae labore ipsam velit ducimus perspiciatis est quidem assumenda nam, error possimus! Eos facilis beatae distinctio, consequuntur voluptatem aspernatur.</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
 </body>
 </html>
